9de0a04cbc8039009a5b0f8d0da29aab7af5bb91
_Anti_ <anti.python.fastapi@gmail.com>
2022-09-26 15:20:00 +0300

    Added make_batch_and_line() in test_batches.py

--- test_batches.py
+++ test_batches.py
@@ -3,6 +3,12 @@ from datetime import date
 from model import Batch, OrderLine
 
 
+def make_batch_and_line(sku: str, batch_qty: int, line_qty: int) -> tuple[Batch, OrderLine]:
+    batch = Batch('batch-001', sku, batch_qty, eta=date.today())
+    line = OrderLine('order-123', sku, line_qty)
+    return batch, line
+
+
 def test_allocating_to_a_batch_reduces_the_available_quantity():
     batch = Batch('batch-001', 'SMALL-TABLE', qty=20, eta=date.today())
     line = OrderLine('order-ref', 'SMALL-TABLE', 2)
